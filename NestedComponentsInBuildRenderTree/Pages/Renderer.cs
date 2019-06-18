0da5e100b5937dc4ccbf98f401a1a8af265acfcf
Joseph Garrone <josephgarrone@gmail.com>
2019-06-18 22:42:04 +1000

    Commit MPE of Nested Components In Build Render Tree causing exception

---
+++ NestedComponentsInBuildRenderTree/Pages/Renderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Layouts;
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace NestedComponentsInBuildRenderTree.Pages
+{
+    public class Renderer : LayoutComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenComponent(1, typeof(SomeControl));
+            // Comment out lines to remove error
+            builder.OpenComponent(2, typeof(SomeControl));
+            builder.CloseComponent();
+            // End of lines to comment out
+            builder.CloseComponent();
+        }
+    }
+}
